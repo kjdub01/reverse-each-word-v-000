ee9be2db01f3f6646081af341d4c9e020ab1266c
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-30 18:14:11 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,8 @@ def reverse_each_word(sentence)
   first_array = sentence.split
   second_array = []
   first_array.each do |string|
-    second_array << 
+    second_array << string.reverse
+  end
+  second_array.join(" ")
     
 end
