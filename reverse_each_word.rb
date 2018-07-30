81de0304002058a50e8441d795e724658108f032
Kate Jewett-Williams <kjewettwilliams@gmail.com>
2018-07-30 17:50:37 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,11 +1,3 @@
 def reverse_each_word(sentence)
-  sentence
-end
-
-def reverse_each_word(sentence)
-  sentence.split
-end
-
-def reverse_each_word(sentence)
-  sentence.split.collect
+  sentence.reverse!
 end
